5af7aeebe5d4750e909963dd462485d146ccebcb
Aditya <adiprofitcoder@gmail.com>
2025-07-28 16:39:49 +0530

    Add files via upload

---
+++ 01. UI basics/main.py
@@ -0,0 +1,11 @@
+'''PyCOD3 | @py.cod3'''
+
+# ? Importing libraries
+import flet as ft
+
+# ! The main function
+def main(page: ft.Page):
+    page.title = "Basic app window"
+
+# ! Run the app
+ft.app(main)
